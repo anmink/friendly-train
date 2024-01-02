9e65e7656d9e53618200ee66c86e2830006cb3f2
mikan94 <hallo.minkenberg@gmail.com>
2024-01-02 20:55:41 +0100

    update app.py

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
 from flask import Flask, request, jsonify
 from rembg import remove
-from flask_cors import CORS
-from PIL import Image
-import cv2
 
 app = Flask(__name__)
-CORS(app)
 
 print('hi')
 
 def remove_background(image):
-    print('start')
-    input_path = image
-    updated_input_path = input_path[7:]
-    output_path = '/Users/anne/Desktop/Ordner/Code/dressflow/images'
-    print(updated_input_path)
-    input = cv2.imread(updated_input_path)
-    print('failed')
-    output = remove(input)
-    cv2.imwrite(output_path, output)
+  print("start")
+  input_path = 'input.png'
+  output_path = 'output.png'
+
+  with open(input_path, 'rb') as i:
+      with open(output_path, 'wb') as o:
+          input = i.read()
+          output = remove(input)
+          o.write(output)
 
 @app.route('/', methods=['GET'])
 def get():
@@ -36,5 +32,5 @@ def image_processing():
     print('except')
     return jsonify({'error': str(e)})
   
-if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+#if __name__ == '__main__':
+#  app.run(debug=True, host='0.0.0.0')
