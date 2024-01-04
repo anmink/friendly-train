1ee813d4b483e45fc985454196d79d428c5d66fe
mikan94 <hallo.minkenberg@gmail.com>
2024-01-04 23:06:40 +0100

    change code

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask, request, jsonify
 from rembg import remove
+import numpy as np
+from PIL import Image
+import cv2
 
 app = Flask(__name__)
 
 print('hi')
 
 def remove_background(image):
-  print("start")
-  print(image)
   input_path = image
-  output_path = 'output.png'
+  print('rembg', input_path)
+  output_path = 'file:///var/mobile/Containers/Data/Application/EB7F7691-6BE9-48A6-A9F9-93FA70E0DB45/Library/Caches/ExponentExperienceData/%2540anonymous%252Fdressflow-1d3c3221-3739-4bb6-8c2b-6c7f07405045/out.png'
 
-  with open(input_path, 'rb') as i:
-      with open(output_path, 'wb') as o:
-          input = i.read()
-          output = remove(input)
-          o.write(output)
+  input = Image.open(input_path)
+  print('after input')
+  output = remove(input)
+  output.save(output_path)
+  print('done in rembg')
+  return output
 
 @app.route('/', methods=['GET'])
 def get():
@@ -24,14 +27,17 @@ def get():
 @app.route('/remove', methods=['POST'])
 def image_processing():
   try:
+    print('try start')
+    #image = '/Users/anne/Library/Developer/CoreSimulator/Devices/9FF48E96-D0C1-401C-876B-58A3E766DBE6/data/Containers/Data/Application/483991C6-B38A-46A2-8DB6-DE00D6D55056/Library/Caches/ExponentExperienceData/%2540anonymous%252Fdressflow-1d3c3221-3739-4bb6-8c2b-6c7f07405045/IMG_4919.jpg'
+    #image = '/Users/anne/Desktop/Kleidung/IMG_4919.jpg'
     data = request.json
-    image = data.get('image')
-    remove_background(image)
-    print(image)
-    return ('work done')
+    image_path = data.get('image')
+    print('print image_path post:', image_path)
+    remove_background(image_path)
+    return ('done post')
   except Exception as e:
     print('except')
-    return jsonify({'error': str(e)})
+    return jsonify({'error call': str(e)})
   
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port='10000')
+  app.run(debug=True, host='0.0.0.0', port='8000')
